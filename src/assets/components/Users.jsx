ddfde8e3438a33a021924da6467c7d81b98f3ff3
Manish Jha <manishjha962@gmail.com>
2025-01-27 17:54:50 +0530

    refactor Users component to use dispatch for user deletion

--- src/assets/components/Users.jsx
+++ src/assets/components/Users.jsx
@@ -1,18 +1,17 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { handleDel } from '../store/usersSlice'
 
 const Users = () => {
-    const { users } = useSelector(state => state.Users)
+    const users = useSelector(state => state.Users)
     console.log(users)
-    const handleDel = (i) => {
-        console.log(i)
-    }
+    const dispatch = useDispatch()
     return (
         <div className='bg-zinc-600 max-w-5xl mx-auto p-4'>
             <h2 className='text-3xl mb-4 underline underline-offset-8 font-semibold'>Users List :</h2>
             {users.map((user, index) => (
                 <div key={index} className='flex gap-4'>
                     <h3>{user.name.firstname}</h3>
-                    <p onClick={() => handleDel(index)} className='text-red-600 cursor-pointer'>x</p>
+                    <p onClick={() => dispatch(handleDel(index))} className='text-red-600 cursor-pointer'>x</p>
                 </div>
             ))}
         </div>
