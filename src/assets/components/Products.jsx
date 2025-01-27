866390728b65f3bc1e1293deab89350a9a1faf07
Manish Jha <manishjha962@gmail.com>
2025-01-27 21:48:01 +0530

    update Products component to display loading message when no products are available

--- src/assets/components/Products.jsx
+++ src/assets/components/Products.jsx
@@ -13,12 +13,12 @@ const Products = () => {
     return (
         <div className='bg-zinc-600 max-w-5xl mx-auto p-4'>
             <h2 className='text-3xl mb-4 underline underline-offset-8 font-semibold'>Products List :</h2>
-            {data.map((product, index) => (
+            {data.length > 0 ? data.map((product, index) => (
                 <div key={index} className='flex gap-4'>
                     <h3>{product.title}</h3>
                     <p onClick={() => dispatch(handleDelProduct(index))} className='text-red-600 cursor-pointer'>x</p>
                 </div>
-            ))}
+            )) : <p>Loading...</p>}
         </div>
     )
 }
